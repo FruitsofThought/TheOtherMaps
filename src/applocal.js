1e87b62aeacc8eb12068b8650bac41faebffcfce
Reinier Battenberg <reinier@mountbatten.nl>
2016-07-21 17:04:27 +0200

    Updated info pane with more info

--- src/applocal.js
+++ src/applocal.js
@@ -1,5 +1,5 @@
 requirejs.config({
-  urlArgs: "bust=1", // + (new Date()).getTime(), this kills javascript debugging
+  urlArgs: "bust=2", // + (new Date()).getTime(), this kills javascript debugging
   baseUrl: '',
   optimizeAllPluginResources: true,
   enforceDefine: false, // will fail for yaml & json anyways nice for debugging
