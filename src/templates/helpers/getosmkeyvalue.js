0e66706c0fbca5ba4e0c43005cfb56bc31af212c
Reinier Battenberg <reinier@mountbatten.nl>
2016-12-07 20:29:53 +0100

    removed logging from osmkeyvalue handlebars helper

--- src/templates/helpers/getosmkeyvalue.js
+++ src/templates/helpers/getosmkeyvalue.js
@@ -1,32 +1,22 @@
 define('templates/helpers/getosmkeyvalue', ['handlebars'], function(
   Handlebars) {
   function getosmkeyvalue(context, options) {
-    /*    console.log(
-          'This : ', this);
-        console.log(
-          'Context : ', context
-        );
-        console.log(
-          'Options : ', options);
-    */
     var wikiproperty;
     var optionsarray = context.split('.');
     var key;
     var value = options.data._parent.root.keyvalues;
-    console.log("value  ", value);
     var arrayLength = optionsarray.length;
 
 
     for (var i = 0; i < arrayLength; i++) {
       value = value[optionsarray[i]];
-      console.log("at key " + i, value);
       if (typeof value == 'undefined') {
         console.error("There is no OSMKeyValue defined for " + context);
         return "";
       }
       // this trick sets the last parents wikiproperty
       // so if your parent is P149 and you do not have a
-      //property set, then we will add your parents property
+      // property set, then we will add your parents property
       if (typeof value.wikiproperty !== 'undefined') {
         wikiproperty = value.wikiproperty;
       }
@@ -34,7 +24,6 @@ define('templates/helpers/getosmkeyvalue', ['handlebars'], function(
     if ((typeof value.wikiproperty == 'undefined') && (typeof value.wikidata !== 'undefined')) {
       value.wikiproperty = wikiproperty;
     }
-    console.log("returning ", value);
 
     return value;
   }
